484369aca38f8c6af61974070139908943aa6d7f
PriyanshuNayak09 <163439750+PriyanshuNayak09@users.noreply.github.com>
2024-05-27 18:47:53 +0530

    used switch case

--- All_arithmetic_operations.cpp
+++ All_arithmetic_operations.cpp
@@ -3,14 +3,29 @@ using namespace std;
 int main()
 {
     int a,b;
+    char c;
     cout<<"enter the 1st no.: ";
     cin>>a;
     cout<<"enter the 2nd no.:";
     cin>>b;
-    cout<<"The sum of the two numbers is "<<a+b<<endl;
-    cout<<"The difference of the two numbers is "<<a-b<<endl;
-    cout<<"The product of the two numbers is "<<a*b<<endl;
-    cout<<"The qoutient upon division of the two numbers is "<<a/b<<endl;
-    cout<<"The remainder upon division of the two numbers is "<<a%b<<endl;
+    cout<<"enter the desired operation: ";
+    cin>>c;
+    switch(c){
+        case('+'):
+            cout<<"The sum of the two numbers is "<<a+b<<endl;
+            break;
+        case('-'):
+            cout<<"The difference of the two numbers is "<<a-b<<endl;
+            break;
+        case('x'):
+            cout<<"The product of the two numbers is "<<a*b<<endl;
+            break;
+        case('/'):
+            cout<<"The qoutient upon division of the two numbers is "<<a/b<<endl;
+            break;
+        case('%'):
+            cout<<"The remainder upon division of the two numbers is "<<a%b<<endl;
+            break;
+    }
     return 0;
 }
